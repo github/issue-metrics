2502efe4ad7abe6df80182eb7cb4131e50a8dc6c
Zack Koppert <zkoppert@github.com>
2023-06-01 23:55:03 -0700

    clean up and formatting
    Signed-off-by: Zack Koppert <zkoppert@github.com>

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
+.PHONY: test
 test:
-	pytest -v --cov=. --cov-fail-under=80
+	pytest -v --cov=. --cov-config=.coveragerc --cov-fail-under=80 --cov-report term-missing
+
+.PHONY: clean
+clean:
+	rm -rf .pytest_cache .coverage __pycache__
--- test_issue_metrics.py
+++ test_issue_metrics.py
@@ -1,140 +1,62 @@
-"""Unit tests for the issue-metrics script.
+"""Unit tests for the issue_metrics module.
 
-This module contains unit tests for the functions in the issue-metrics script.
-The tests use the unittest module and the MagicMock and patch classes from the
-unittest.mock module to mock the GitHub API and test the functions in isolation.
+This module contains unit tests for the functions in the issue_metrics module.
+The tests use the unittest module and the unittest.mock module to mock the
+GitHub API and test the functions in isolation.
 
 Classes:
-    TestSearchIssues: Test the search_issues function.
-    TestAuthToGithub: Test the auth_to_github function.
-    TestMeasureTimeToFirstResponse: Test the measure_time_to_first_response function.
-    TestGetAverageTimeToFirstResponse: Test the get_average_time_to_first_response function.
-    TestWriteToMarkdown: Test the write_to_markdown function.
-
+    TestSearchIssues: A class containing unit tests for the search_issues function.
+    TestMeasureTimeToFirstResponse: A class containing unit tests for the
+        measure_time_to_first_response function.
+    TestGetAverageTimeToFirstResponse: A class containing unit tests for the
+        get_average_time_to_first_response function.
+    TestWriteToMarkdown: A class containing unit tests for the write_to_markdown function.
 """
-import os
 import unittest
-from datetime import timedelta
+import os
 from unittest.mock import MagicMock, patch
-
+from datetime import timedelta, datetime
+import issue_metrics
 from issue_metrics import (
-    auth_to_github,
-    get_average_time_to_first_response,
-    measure_time_to_first_response,
     search_issues,
+    auth_to_github,
+    measure_time_to_first_response,
+    get_average_time_to_first_response,
     write_to_markdown,
 )
 
 
-class TestGetAverageTimeToFirstResponse(unittest.TestCase):
-    """Test the get_average_time_to_first_response function."""
-
-    def test_get_average_time_to_first_response(self):
-        """Test that get_average_time_to_first_response calculates the correct average.
-
-        This test creates a list of mock GitHub issues with time to first response
-        attributes, calls get_average_time_to_first_response with the list, and
-        checks that the function returns the correct average time to first response.
-
-        """
-        # Create a list of mock GitHub issues with time to first response attributes
-        issues = [
-            MagicMock(time_to_first_response=timedelta(days=1)),
-            MagicMock(time_to_first_response=timedelta(hours=13, minutes=30)),
-            MagicMock(time_to_first_response=timedelta(hours=4, minutes=30)),
-        ]
-
-        # Call get_average_time_to_first_response with the list of issues
-        average_time_to_first_response = get_average_time_to_first_response(issues)
-
-        # Check that the function returns the correct average time to first response
-        expected_average_time_to_first_response = timedelta(hours=14)
-        self.assertEqual(
-            average_time_to_first_response,
-            expected_average_time_to_first_response.seconds,
-        )
-
-
-class TestWriteToMarkdown(unittest.TestCase):
-    """Test the write_to_markdown function."""
-
-    def test_write_to_markdown(self):
-        """Test that write_to_markdown writes the correct output to a file.
-
-        This test creates a temporary file, calls write_to_markdown with a list of
-        issues and their time to first response, and checks that the file contains
-        the expected output.
-
-        """
-        # Create a temporary file
-        with open("temp.md", "w", encoding="utf-8") as file:
-            file.write("")
-
-        # Call write_to_markdown with a list of issues and their time to first response
-        issues_with_metrics = [
-            ("Issue 1", "1 day, 2:30:00"),
-            ("Issue 2", "3 days, 4:30:00"),
-            ("Issue 3", "0:30:00"),
-        ]
-        average_time_to_first_response = "1 day, 3:10:00"
-        write_to_markdown(
-            issues_with_metrics,
-            average_time_to_first_response,
-            file=open("temp.md", "w", encoding="utf-8"),
-        )
-
-        # Check that the file contains the expected output
-        with open("temp.md", "r", encoding="utf-8") as file:
-            output = file.read()
-        expected_output = (
-            "# Issue Metrics\n\n"
-            "Average time to first response: 1 day, 3:10:00\n"
-            "Number of issues: 3\n\n"
-            "| Issue | TTFR |\n"
-            "| --- | ---: |\n"
-            "| Issue 2 | 3 days, 4:30:00 |\n"
-            "| Issue 1 | 1 day, 2:30:00 |\n"
-            "| Issue 3 | 0:30:00 |\n"
-        )
-        self.assertEqual(output, expected_output)
-
-        # Remove the temporary file
-        os.remove("temp.md")
-
-
 class TestSearchIssues(unittest.TestCase):
-    """Test the search_issues function."""
+    """Unit tests for the search_issues function.
 
-    @patch("github3.login")
-    def test_search_issues(self, mock_login):
-        """Test that search_issues returns the correct issues.
+    This class contains unit tests for the search_issues function in the
+    issue_metrics module. The tests use the unittest module and the unittest.mock
+    module to mock the GitHub API and test the function in isolation.
 
-        This test mocks the GitHub connection and repository, and checks that
-        search_issues returns the correct issues.
+    Methods:
+        test_search_issues: Test that search_issues returns the correct issues.
 
-        """
-        # Mock the GitHub connection
-        mock_gh = MagicMock()
-        mock_login.return_value = mock_gh
+    """
 
-        # Mock the repository and issues
-        mock_repo = MagicMock()
-        mock_issue1 = MagicMock(title="Test issue 1")
-        mock_issue2 = MagicMock(title="Test issue 2")
-        mock_repo.search_issues.return_value = [mock_issue1, mock_issue2]
-        mock_gh.repository.return_value = mock_repo
+    @patch("issue_metrics.urlparse")
+    def test_search_issues(self, mock_urlparse):
+        """Test that search_issues returns the correct issues."""
+        # Set up the mock GitHub connection object
+        mock_connection = MagicMock()
+        mock_issues = [
+            MagicMock(title="Issue 1"),
+            MagicMock(title="Issue 2"),
+        ]
+        mock_connection.search_issues.return_value = mock_issues
 
-        # Call the function
+        # Set up the mock urlparse function
+        mock_urlparse.return_value.path = "/user/repo"
+
+        # Call search_issues and check that it returns the correct issues
         issues = search_issues(
-            "https://github.com/octocat/hello-world", "is:open is:issue", mock_gh
+            "https://github.com/user/repo", "is:open", mock_connection
         )
-
-        # Check the results
-        self.assertEqual(len(issues), 2)
-        self.assertEqual(issues[0].title, "Test issue 1")
-        self.assertEqual(issues[1].title, "Test issue 2")
-        mock_gh.repository.assert_called_once_with("octocat", "hello-world")
-        mock_repo.search_issues.assert_called_once_with("is:open is:issue")
+        self.assertEqual(issues, mock_issues)
 
 
 class TestAuthToGithub(unittest.TestCase):
@@ -163,20 +85,6 @@ class TestAuthToGithub(unittest.TestCase):
         self.assertEqual(github_connection, mock_gh)
         mock_login.assert_called_once_with(token="test_token")
 
-    def test_auth_to_github_without_token(self):
-        """Test that auth_to_github raises an exception without a token.
-
-        This test sets the GH_TOKEN environment variable to an empty string and
-        checks that auth_to_github raises a ValueError exception.
-
-        """
-        # Set up the environment variable
-        os.environ.pop("GH_TOKEN", None)
-
-        # Call the function and check for an exception
-        with self.assertRaises(ValueError):
-            auth_to_github()
-
 
 class TestMeasureTimeToFirstResponse(unittest.TestCase):
     """Test the measure_time_to_first_response function."""
@@ -190,16 +98,20 @@ class TestMeasureTimeToFirstResponse(unittest.TestCase):
         """
         # Set up the mock GitHub issues
         mock_issue1 = MagicMock()
+        mock_issue1.comments = 1
         mock_issue1.created_at = "2023-01-01T00:00:00Z"
+
         mock_comment1 = MagicMock()
-        mock_comment1.created_at = "2023-01-02T00:00:00Z"
-        mock_issue1.comments.return_value = [mock_comment1]
+        mock_comment1.created_at = datetime.fromisoformat("2023-01-02T00:00:00Z")
+        mock_issue1.issue.comments.return_value = [mock_comment1]
 
         mock_issue2 = MagicMock()
+        mock_issue2.comments = 1
         mock_issue2.created_at = "2023-01-03T00:00:00Z"
+
         mock_comment2 = MagicMock()
-        mock_comment2.created_at = "2023-01-04T00:00:00Z"
-        mock_issue2.comments.return_value = [mock_comment2]
+        mock_comment2.created_at = datetime.fromisoformat("2023-01-04T00:00:00Z")
+        mock_issue2.issue.comments.return_value = [mock_comment2]
 
         mock_issues = [mock_issue1, mock_issue2]
 
@@ -208,5 +120,166 @@ class TestMeasureTimeToFirstResponse(unittest.TestCase):
 
         # Check the results
         self.assertEqual(len(issues_with_metrics), 2)
-        self.assertEqual(issues_with_metrics[0].time_to_first_response.days, 1)
-        self.assertEqual(issues_with_metrics[1].time_to_first_response.days, 1)
+        self.assertEqual(issues_with_metrics[0][2], timedelta(days=1))
+        self.assertEqual(issues_with_metrics[1][2], timedelta(days=1))
+
+
+class TestGetAverageTimeToFirstResponse(unittest.TestCase):
+    """Test the get_average_time_to_first_response function."""
+
+    def test_get_average_time_to_first_response(self):
+        """Test that get_average_time_to_first_response calculates the correct average.
+
+        This test creates a list of mock GitHub issues with time to first response
+        attributes, calls get_average_time_to_first_response with the list, and
+        checks that the function returns the correct average time to first response.
+
+        """
+        # Create a list of mock GitHub issues with time to first response attributes
+        issues = [
+            [
+                "Title 1",
+                "https://github.com/owner/repo1",
+                timedelta(days=1, hours=2, minutes=30),
+            ],
+            [
+                "Title 2",
+                "https://github.com/owner/repo2",
+                timedelta(days=3, hours=4, minutes=30),
+            ],
+            ["Title 3", "https://github.com/owner/repo3", timedelta(minutes=30)],
+        ]
+
+        # Call get_average_time_to_first_response with the list of issues
+        average_time_to_first_response = get_average_time_to_first_response(issues)
+
+        # Check that the function returns the correct average time to first response
+        expected_average_time_to_first_response = timedelta(
+            days=1, hours=10, minutes=30
+        )
+        self.assertEqual(
+            average_time_to_first_response, expected_average_time_to_first_response
+        )
+
+
+class TestWriteToMarkdown(unittest.TestCase):
+    """Test the write_to_markdown function."""
+
+    def test_write_to_markdown(self):
+        """Test that write_to_markdown writes the correct markdown file.
+
+        This test creates a list of mock GitHub issues with time to first response
+        attributes, calls write_to_markdown with the list and the average time to
+        first response, and checks that the function writes the correct markdown
+        file.
+
+        """
+        # Create a list of mock GitHub issues with time to first response attributes
+        issues_with_metrics = [
+            (
+                "Issue 1",
+                "https://github.com/user/repo/issues/1",
+                timedelta(days=1, hours=2, minutes=30),
+            ),
+            (
+                "Issue 2",
+                "https://github.com/user/repo/issues/2",
+                timedelta(days=3, hours=4, minutes=30),
+            ),
+            ("Issue 3", "https://github.com/user/repo/issues/3", timedelta(minutes=30)),
+        ]
+
+        # Call write_to_markdown with the list of issues and the average time to first response
+        average_time_to_first_response = timedelta(days=1, hours=3, minutes=10)
+        write_to_markdown(
+            issues_with_metrics, average_time_to_first_response, file=None
+        )
+
+        # Check that the function writes the correct markdown file
+        with open("issue_metrics.md", "r", encoding="utf-8") as file:
+            content = file.read()
+        expected_content = (
+            "# Issue Metrics\n\n"
+            "Average time to first response: 1 day, 3:10:00\n"
+            "Number of issues: 3\n\n"
+            "| Title | URL | TTFR |\n"
+            "| --- | --- | ---: |\n"
+            "| Issue 3 | https://github.com/user/repo/issues/3 | 0:30:00 |\n"
+            "| Issue 2 | https://github.com/user/repo/issues/2 | 3 days, 4:30:00 |\n"
+            "| Issue 1 | https://github.com/user/repo/issues/1 | 1 day, 2:30:00 |\n"
+        )
+        self.assertEqual(content, expected_content)
+
+
+class TestMain(unittest.TestCase):
+    """Unit tests for the main function.
+
+    This class contains unit tests for the main function in the issue_metrics
+    module. The tests use the unittest module and the unittest.mock module to
+    mock the GitHub API and test the function in isolation.
+
+    Methods:
+        test_main: Test that main runs without errors.
+
+    """
+
+    @patch("issue_metrics.auth_to_github")
+    @patch("issue_metrics.search_issues")
+    @patch("issue_metrics.measure_time_to_first_response")
+    @patch("issue_metrics.get_average_time_to_first_response")
+    @patch.dict(
+        os.environ,
+        {
+            "ISSUE_SEARCH_QUERY": "is:open",
+            "REPOSITORY_URL": "https://github.com/user/repo",
+        },
+    )
+    def test_main(
+        self,
+        mock_get_average_time_to_first_response,
+        mock_measure_time_to_first_response,
+        mock_search_issues,
+        mock_auth_to_github,
+    ):
+        """Test that main runs without errors."""
+        # Set up the mock GitHub connection object
+        mock_connection = MagicMock()
+        mock_auth_to_github.return_value = mock_connection
+
+        # Set up the mock search_issues function
+        mock_issues = [
+            MagicMock(title="Issue 1"),
+            MagicMock(title="Issue 2"),
+        ]
+        mock_search_issues.return_value = mock_issues
+
+        # Set up the mock measure_time_to_first_response function
+        mock_issues_with_ttfr = [
+            (
+                "Issue 1",
+                "https://github.com/user/repo/issues/1",
+                timedelta(days=1, hours=2, minutes=30),
+            ),
+            (
+                "Issue 2",
+                "https://github.com/user/repo/issues/2",
+                timedelta(days=3, hours=4, minutes=30),
+            ),
+        ]
+        mock_measure_time_to_first_response.return_value = mock_issues_with_ttfr
+
+        # Set up the mock get_average_time_to_first_response function
+        mock_average_time_to_first_response = 15
+        mock_get_average_time_to_first_response.return_value = (
+            mock_average_time_to_first_response
+        )
+
+        # Call main and check that it runs without errors
+        issue_metrics.main()
+
+        # Remove the markdown file created by main
+        os.remove("issue_metrics.md")
+
+
+if __name__ == "__main__":
+    unittest.main()
