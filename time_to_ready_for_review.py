911d0800388c417d62b8072af570b7e29cb21bd2
Zack Koppert <zkoppert@github.com>
2023-09-11 16:13:44 -0700

    linting fixes
    Signed-off-by: Zack Koppert <zkoppert@github.com>

--- time_to_ready_for_review.py
+++ time_to_ready_for_review.py
@@ -19,13 +19,13 @@ from typing import Union
 
 import github3
 
+
 def get_time_to_ready_for_review(
-    issue: github3.issues.Issue,
-    pull_request: github3.pulls.PullRequest
+    issue: github3.issues.Issue, pull_request: github3.pulls.PullRequest
 ) -> Union[datetime, None]:
     """If a pull request was formerly a draft, get the time it was marked as ready
     for review
-    
+
     Args:
         issue (github3.issues.Issue): A GitHub issue.
         pull_request (github3.pulls.PullRequest): A GitHub pull request.
@@ -38,7 +38,7 @@ def get_time_to_ready_for_review(
 
     events = issue.issue.events(number=50)
     for event in events:
-        if event.event == 'ready_for_review':
+        if event.event == "ready_for_review":
             return event.created_at
 
     return None
