2cdc7eac312a2fe236695842570f493f7b4f5c5d
Zack Koppert <zkoppert@github.com>
2024-04-16 16:09:28 -0700

    chore: use tabs in bash scripts
    Signed-off-by: Zack Koppert <zkoppert@github.com>

--- .github/scripts/env_vars_check.sh
+++ .github/scripts/env_vars_check.sh
@@ -8,15 +8,15 @@ NC='\033[0m' # No Color
 
 # Loop through each file
 for file in $files; do
-     # Search for instances of get_env_vars() with no arguments
-     result=$(grep -n "get_env_vars()" "$file")
+	# Search for instances of get_env_vars() with no arguments
+	result=$(grep -n "get_env_vars()" "$file")
 
-     # If any instances are found, print the file name and line number
-     if [ -n "$result" ]; then
-         echo "Found in $file:"
-         echo "$result"
-         echo -e "${RED}ERROR: get_env_vars() should always set test=True in test*.py files.${NC}"
-         exit 1
-     fi
+	# If any instances are found, print the file name and line number
+	if [ -n "$result" ]; then
+		echo "Found in $file:"
+		echo "$result"
+		echo -e "${RED}ERROR: get_env_vars() should always set test=True in test*.py files.${NC}"
+		exit 1
+	fi
 done
 echo -e "   ${GREEN}PASS:${NC} All test*.py files call get_env_vars() with test=True."
